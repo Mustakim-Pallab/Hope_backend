d1ac847d2aae1604b7c362ef8b953ee4c9f5d22b
A. S. M Mustakim Rahman Siddique <mustakimpallab2434@gmail.com>
2023-05-16 06:33:34 +0600

    server side is cooking

--- index.js
+++ index.js
@@ -4,36 +4,90 @@ require("dotenv").config()
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const path= require('path')
+const crypto = require('crypto');
+
+let therapistIamgeName;
+const multer = require('multer')
+const storage = multer.diskStorage({
+	destination:(req,file,call_back)=>{
+		call_back(null,'images')
+	},
+	filename:(req,file, call_back) =>{
+		console.log(file)
+		therapistIamgeName= Date.now()+crypto.randomBytes(20).toString('hex')+'therapist'+path.extname(file.originalname);
+		call_back(null,therapistIamgeName)
+	}
+})
+const upload = multer({storage})
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(cors())
 
-app.post("/payment", cors(), async (req, res) => {
-	let { amount, id } = req.body
-	try {
-		const payment = await stripe.paymentIntents.create({
-			amount,
-			currency: "BDT",
-			description: "Pay for books",
-			payment_method: id,
-			confirm: true
-		})
-		console.log("Payment", payment)
-		res.json({
-			message: "Payment successful",
-			success: true
-		})
-	} catch (error) {
-		console.log("Error", error)
-		res.json({
-			message: "Payment failed",
-			success: false
-		})
-	}
-})
+const uri = `mongodb+srv://${process.env.REACT_APP_MONGODB_USER}:${process.env.REACT_APP_MONGODB_PASS}@cluster0.iscr21w.mongodb.net/?retryWrites=true&w=majority`
 
-app.listen(process.env.PORT || 4000, () => {
-	console.log("Sever is listening on port 4000")
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+
+	try {
+  
+	  const database = client.db("Hope");
+  
+	  const numberCollection = database.collection("number");
+	  const therapistCollection = database.collection("therapists");
+  
+	  
+  
+	  app.post('/number', async(req,res) =>{
+		const number= req.body;
+		console.log(number);
+		const result = await numberCollection.insertOne(number);
+		res.send(result)
+		
+	  })
+	  app.post('/upload',upload.single('image'), async(req,res) =>{
+		const therapistDetails= req.body;
+		// const{image} = req.files;
+		// const imagename=Date.now()+crypto.randomBytes(20).toString('hex')+'therapist'+path.extname(file.originalname)
+		// console.log(number);
+		const result = await therapistCollection.insertOne({
+			therapistDetails,
+			therapistIamgeName
+		});
+		res.send(therapistIamgeName);
+		
+	  })
+
+	  app.get('/number', async(req,res) =>{
+		const query= {};
+		const cursor = numberCollection.find(query);
+		const result = await cursor.toArray();
+		res.send(result)
+		
+	  })
+
+	  
+	
+  
+	} finally {
+  
+	//   await client.close();
+  
+	}
+  
+  }
+  
+  run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+  })
+  
+app.listen(process.env.PORT || 5000, () => {
+	console.log("Sever is listening on port 5000")
 })
